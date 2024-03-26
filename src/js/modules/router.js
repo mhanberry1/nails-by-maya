284bf3863e643577a25b57b3a2ecaf484c45d701
Madison Hanberry <mhanberry@protonmail.com>
2024-03-26 00:34:02 -0700

    refresh routes when new page is loaded

--- src/js/modules/router.js
+++ src/js/modules/router.js
@@ -7,22 +7,25 @@ export const createRoutes = () => {
 
 	$$('a').forEach( async a => {
 		if (!a.href) return
-		if (routes[a.href]) return
-		if (!a.href.includes(`${location.protocol}//${location.hostname}`)) return
 
 		const href = a.href
 		const prev = a.onclick
-		const response = await fetch(href)
 
-		routes[href] = parser.parseFromString(await response.text(), 'text/html')
+		a.removeAttribute('href')
 		a.onclick = () => {
 			prev && prev()
 			go(href)
 		}
-		a.removeAttribute("href")
+
+		if (routes[href]) return
+		if (!href.includes(`${location.protocol}//${location.hostname}`)) return
+
+		const response = await fetch(href)
+
+		routes[href] = parser.parseFromString(await response.text(), 'text/html')
 	})
 
-	addEventListener("popstate", ({ state }) => go(state.href))
+	addEventListener('popstate', ({ state }) => go(state.href))
 
 	return routes
 }
@@ -34,6 +37,8 @@ export const go = href => {
 
 	$('head').innerHTML = routes[href].querySelector('head').innerHTML
 	$('main').innerHTML = routes[href].querySelector('main').innerHTML
+
+	createRoutes()
 
 	link.hash
 		? $(link.hash)?.scrollIntoView()
